b4db75050e95b0efa98f8d123d235248a7659b36
zezeon <zezeon@msn.com>
2017-05-31 14:40:07 +0900

    ++

--- 3.m
+++ 3.m
@@ -1,9 +1,14 @@
 function F = fourier(w)
 t = [-4:0.01:4];
-F = cumsum(exp(-j * w .* t)) * 0.01;
+F = sum(exp(-j * w * t)) * 0.01;
 end
 
-w = linspace(-2*pi, 2*pi, length(t));
-y = fourier(w);
+l = length(t)
+w = linspace(-2*pi, 2*pi, l);
+y = w;
+for n= [1:l]
+y(n) = fourier(w(n));
+end
 plot(w, abs(y));
+
 
