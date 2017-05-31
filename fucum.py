69bf7d4bb17d26f438cb278ee2c2644f9ae04198
zezeon <zezeon@msn.com>
2017-05-31 15:10:06 +0900

    ++b

--- fucum.py
+++ fucum.py
@@ -5,14 +5,13 @@ dt = 0.01
 t = np.arange(-5, 5, dt);
 x = np.e **(-5 * abs(t));
 def fourier(w):
-    dt = 0.01
     return np.sum(x * np.e**(-1j * w * t)) * dt
 
 l = t.size;
 w = np.linspace(-2 * np.pi, 2 * np.pi, l);
 y = w;
 for i in range(l):
-    y[i] = abs(fourier(w[i]));
+    y[i] = abs(fourier(y[i]));
 
 plt.plot(w, y);
 plt.show();
