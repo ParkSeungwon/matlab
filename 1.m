c52346a2f1d056f26d7a72fe46f41a4d54816142
zezeon <zezeon@msn.com>
2017-06-05 11:24:32 +0900

    -

--- 1.m
+++ 1.m
@@ -1,4 +1,20 @@
-w = [-2*pi:0.005:2*pi];
-y = 20*sin(5*w)./w;
-z = 20*sin(25*w)./w;
-plot(w, y, w, z)
+a1 = 1/3;
+max_limit = 10;
+for n=[0:max_limit-1]
+	x1(n+1) = a1^n;
+end
+
+n = [0:max_limit-1];
+Wmax = 2*pi;
+W =  n*Wmax/ max_limit;
+L = length(W);
+clear XW;
+XW = zeros(L);
+for k=[1:1:L]
+	XW(k) = x1 * exp(-j*n*W(k));
+end
+
+XW_Mag = abs(XW);
+plot(W, XW_Mag)
+XW_Phase = phase(XW);
+plot(W, XW_Phase)
